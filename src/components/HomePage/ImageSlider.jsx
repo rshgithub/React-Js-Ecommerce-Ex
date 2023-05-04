e613fd8755e4369378dd2722a185e94c95cd36d6
haneen almasry <haneenmasry0@gmail.com>
2023-05-04 15:38:57 +0300

    products-grid

--- src/components/HomePage/ImageSlider.jsx
+++ src/components/HomePage/ImageSlider.jsx
@@ -3,71 +3,70 @@ import { Carousel,Row,Col,Image} from 'antd';
 import Iphone from '../../assets/MaskGroup1.png'
 import "../HomePage.css";
 
-const ImageSlider = (props) => {
-  const [dotPosition, setDotPosition] = useState('bottom');
-  const handlePositionChange = ({ target: { value } }) => {
-    setDotPosition(value);
-  };
-  return (
-   <>
-      <Carousel dotPosition={dotPosition} autoplay
-     style={{backgroundImage: "linear-gradient(to right, rgba(255,0,0), rgba(213,83,151),rgba(153,98,240))",}}>
-      <Row>
-        <Col
-          span={12}
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            
-          }}
-        >
-          <h2 style={{ color: 'white' , fontSize: 36 , fontWeight:"bold" ,  }} >iPhone X</h2>
-          <p style={{ color: 'white' , fontSize: 14 }} >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.
-            </p> 
-          <a href="#" style=
-          {{color: 'white' ,fontWeight:"bold"}}>MORE</a>
-          <Image
-          style={{
-            display: "flex",
-            justifyContent: "left",
-            alignItems: "left",}}
-            src={Iphone}
-            height="400px"
-            width="400px"
-            preview={false}/>
-        </Col>
-      
-            {/* <Col span={6}>
-        <Image
-          style={{
-            display: "flex",
-            justifyContent: "right",
-            alignItems: "right",}}
-            src={Iphone}
-            height="400px"
-            width="400px"
-            preview={false}/>
-            </Col> */}
-        {/*<Image src={Banner} style={{
-          position: "absolute",
-           alignItems: "center",
-           justifyContent: "center",
-           flexWrap: "wrap"
-        }} 
-      preview={false}/>*/}
+const ImageSlider = () => {
+const [dotPosition, setDotPosition] = useState('bottom');
 
-      </Row>
-      </Carousel>
+return (
+<>
+<Carousel dotPosition={dotPosition}
+style={{backgroundImage: "linear-gradient(to right, rgba(255,0,0), rgba(213,83,151),rgba(153,98,240))",}}>
+<Row>
+<Col
+log={6}
+style={{
+display: "flex",
+flexDirection: "column",
+justifyContent: "center",
+alignItems: "center",
+
+}}
+>
+<h2 style={{ color: 'white' , fontSize: 36 , fontWeight:"bold" ,  }} >iPhone X</h2>
+<p style={{ color: 'white' , fontSize: 14 }} >
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.
+</p> 
+<a href="#" style=
+{{color: 'white' ,fontWeight:"bold"}}>MORE</a>
+<Image
+style={{
+display: "flex",
+justifyContent: "left",
+alignItems: "left",}}
+src={Iphone}
+height="400px"
+width="400px"
+preview={false}/>
+</Col>
+
+</Row>
+<Col log={6}>
+<Image
+style={{
+display: "flex",
+justifyContent: "right",
+alignItems: "right",}}
+src={Iphone}
+height="400px"
+width="400px"
+preview={false}/>
+</Col>
+{/*<Image src={Banner} style={{
+position: "absolute",
+alignItems: "center",
+justifyContent: "center",
+flexWrap: "wrap"
+}} 
+preview={false}/>*/}
 
 
-        
-      </>
-  );
-        }
+</Carousel>
 
-      
-        
- export default ImageSlider;
+
+
+</>
+);
+}
+
+
+
+export default ImageSlider;
